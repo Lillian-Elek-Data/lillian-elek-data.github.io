88353feb5f41db30baea5c15cf891319c3d0917e
Lillian <le732943@gmail.com>
2026-01-05 15:44:19 -0700

    updated links

---
+++ assets/js/links.js
@@ -0,0 +1,23 @@
+// assets/js/links.js
+window.LINKS = {
+  stroke: {
+    project: "stroke_project_page.html",
+    repo: "https://github.com/Lillian-Elek-Data/ml-with-python/blob/main/projects/stroke-prediction-logistic-regression/notebooks/01_stroke_prediction_logistic_regression.ipynb"
+    },
+  weather: {
+    project: "weather_project_page.html",
+    repo: "https://github.com/Lillian-Elek-Data/ml-with-python/blob/main/projects/weather-ml/Weather%20Data%20OLS%20Regression.ipynb"
+    },
+  sql: {
+    project: "",
+    repo: "https://github.com/Lillian-Elek-Data/SQL"
+  },
+  books: {
+    project: "",
+    repo: "https://github.com/Lillian-Elek-Data/ml-with-python/blob/main/projects/books-read-linear-regression/Number%20of%20Books%20Read%20-%20SLR.ipynb"
+  },
+  co2: {
+    project: "",
+    repo: "https://github.com/Lillian-Elek-Data/ml-with-python/blob/main/projects/co2-emissions-ols/ML%20OLS%20Regression%20with%20CO2%20Data.ipynb"
+    }
+};
